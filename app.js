bf76f635ced4c09c759dbf038975a6a3e0b9458e
JaeIn <sm810403@hotmail.com>
2020-07-14 23:49:59 -0700

    third commit
    added constructor

--- app.js
+++ app.js
@@ -4,29 +4,79 @@ const myCanvas = document.querySelector('#myCanvas');
     myCanvas.height = window.innerHeight;
     
 const ctx = myCanvas.getContext('2d');
-    ctx.fillStyle = '#000';
-    ctx.fillRect(100, 100, 80, 80);
+    // ctx.fillStyle = '#000';
+    // ctx.fillRect(100, 100, 80, 80);
     // ctx.strokeStyle = '#d3e';
 
-    var x = Math.random()*innerWidth;
-    var y = Math.random()*innerHeight;
-    var dx = 4;
-    var dy= 4;
-    var radius = 30
+    
+// for (var i=0; i <10; i++) {
+//     var x = Math.random()*window.innerWidth;
+//     var y = Math.random()*window.innerHeight;
+//     var dx = Math.random()*20;
+//     var dy= Math.random()*20;
+//     function animate(){
+//         requestAnimationFrame(animate);
+//         ctx.clearRect(0,0,innerWidth,innerHeight)
+//     ctx.beginPath();
+//     ctx.arc(x,y,40,0,Math.PI*2,false);
+//     ctx.strokeStyle = 'blue';
+//     ctx.stroke();
+//     x+=dx;
+//     y+=dy;
+// }
+// }
+// animate();
+function Circle(x,y,radius,dy,dx){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dy = dy;
+    this.dx = dx;
+
+    this.draw = function(){
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        ctx.strokeStyle = 'red';
+        ctx.stroke();
+    }
+    this.update = function(){
+        if (this.x+this.radius > innerWidth || this.x-this.radius < 0){
+            this.dx = -this.dx;
+           } else if (this.y+this.radius>innerHeight || this.y-this.radius <0){
+            this.dy = -this.dy;
+           }
+           this.x +=this.dx;
+           this.y +=this.dy;
+           this.draw();
+    }
+    
+}
+
+
+var arr = [];
+for (var i =0; i <100;i++ ){
+    var x = Math.random()*window.innerWidth;
+    var y = Math.random()*window.innerHeight;
+    var dx = (Math.random()-0.5);
+    var dy= (Math.random()-0.5);
+    var radius = 30;
+    arr.push(new Circle(x,y,dx,dy,radius));
+}
+
+
     function animate(){
         requestAnimationFrame(animate);
         ctx.clearRect(0,0,innerWidth,innerHeight);
-        ctx.beginPath();
-        ctx.arc(x,y,radius,0,Math.PI*2,false);
-        ctx.strokeStyle = '#d83';
-        ctx.stroke();
-        x +=dx;
-        y +=dy
-        if (x+radius > innerWidth || y+radius>innerHeight){
-         x -= dx;
-         y -= dy;
-        }
+        
+       for (var i=0; i < arr.length; i ++){
+            arr[i].update();
+       }
+        
+
+        
+        
     }
     animate();
+ 
 
     
